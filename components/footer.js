b4c08308933b6210f93ba613863cee1d70225041
Yasir Idriz <yasiridriz@Yasirs-MacBook-Pro.local>
2021-04-21 22:44:41 +0200

    footer fix

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer>
-            ©2021 Të gjitha të drejtat e rezervuara 
+            © 2021 Të gjitha të drejtat e rezervuara 
             {/* With <span style={{color: "red"}}>&hearts;</span> from <a href="instagram.com/yasiridriz">Yasir</a> */}
         </footer>
     )
