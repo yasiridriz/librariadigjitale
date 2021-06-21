2e979afe373ef3fa32dffc85c38f80aca91b6e67
Yasir Idriz <yasiridriz@Yasirs-MacBook-Pro.local>
2021-06-21 21:54:36 +0200

    update boom

--- pages/books.js
+++ pages/books.js
@@ -155,8 +155,13 @@ const Books = () => {
             <motion.div variants={titleVariants} className="search">
                 <div className="row">
                     <div className="col-md-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="40" height="40" viewBox="0 0 24 24" stroke-width="1" stroke="#cccccc" fill="none" stroke-linecap="square" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <circle cx="10" cy="10" r="7" />
+                            <line x1="21" y1="21" x2="15" y2="15" />
+                        </svg>
                         <span className="input input--nao">
-                            <input className="input__field input__field--nao" type="text" value={search} onChange={e => setSearch(e.target.value)} id="input-1" placeholder="Kërko" />
+                            <input className="input__field input__field--nao" type="text" value={search} onChange={e => setSearch(e.target.value)} id="input-1" placeholder='"Distopi"' />
                             <svg className="graphic graphic--nao" width="300%" height="100%" viewBox="0 0 1200 60"
                                 preserveAspectRatio="none">
                                 <path
