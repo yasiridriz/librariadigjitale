ec074ec0eed6d17c958d6475f650eab9e8f74114
Yasir Idriz <yasiridriz@Yasirs-MacBook-Pro.local>
2021-09-06 11:40:59 +0200

    Books stored in Google Drive

--- pages/books.js
+++ pages/books.js
@@ -6,9 +6,9 @@ import { titleVariants, contentVariants } from '../components/motionVariants';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
-import clientPromise from "../lib/mongodb";
 
-import books from '../books.json';
+import clientPromise from "../lib/mongodb"; // mongo client
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
 const bookVariants = {
     enter: {
@@ -41,30 +41,26 @@ function ExpandedBook({ book, onCollapse }) {
                                 <div className="row">
                                     <div className="col-md-4">
                                         <motion.div layoutId={`image-${book.id}`} className="imageContainer">
-                                            <img layoutId={`image-${book.id}`} src={book.image} />
+                                            <img src={book.image} />
                                         </motion.div>
                                     </div>
                                     <div className="col-md-8">
                                         <motion.div layoutId={`details-${book.id}`} className="details">
                                             <span className="close" onClick={onCollapse}></span>
                                             <h1>{book.title}</h1>
-                                            <p>Nga <Link href="/authors/id"><a>{book.author}</a></Link></p>
+                                            <p>Nga {book.author}</p>
                                             <motion.div className="description">
                                                 <p>
                                                     {book.description}
                                                 </p>
                                                 <hr />
-                                                <br />
-                                                <div class="links">
-                                                    <a target="_blank" href={book.link} className="btn-main noborder" >
-                                                        Lexo {book.publisher !== "" ? `në ${book.publisher}` : ""}
-                                                    </a>
-                                                    <a target="_blank" download href={book.link} className="btn-main noborder" >
-                                                        PDF
-                                                    </a>
-                                                    <a target="_blank" download href={book.link} className="btn-main noborder" >
-                                                        EPUB
-                                                    </a>
+                                                <div className="links">
+                                                    <p> Shkarko: </p>
+                                                    {book.links.map(link => (
+                                                        <a target="_blank" download href={link.link} className="btn-main noborder" >
+                                                            {link.type} {book.publisher !== "" ? `në ${book.publisher}` : ""}
+                                                        </a>
+                                                    ))}
 
                                                 </div>
 
@@ -151,23 +147,23 @@ const Books = ({ books }) => {
         <motion.div initial="initial" animate="enter" exit="exit" variants={titleVariants} className="container">
             <motion.h1 initial="initial" animate="enter" exit="exit" variants={titleVariants} className="bigtitle"><span>Të Gjitha Librat</span></motion.h1>
             <motion.div variants={titleVariants} className="search">
-                <div className="row">
-                    <div className="col-md-6">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-search" width="40" height="40" viewBox="0 0 24 24" strokeWidth="1" stroke="#cccccc" fill="none" strokeLinecap="square" strokeLinejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                            <circle cx="10" cy="10" r="7" />
-                            <line x1="21" y1="21" x2="15" y2="15" />
+                <div className="group">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-search" width="30" height="45" viewBox="0 0 24 24" strokeWidth="1" stroke="#bbb" fill="none" strokeLinecap="square" strokeLinejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <circle cx="10" cy="10" r="7" />
+                        <line x1="21" y1="21" x2="15" y2="15" />
+                    </svg>
+                    <span className="input input--nao">
+                        <input className="input__field input__field--nao" type="text" value={search} onChange={e => setSearch(e.target.value)} id="input-1" placeholder='"Distopi"' />
+                        <svg className="graphic graphic--nao" width="300%" height="100%" viewBox="0 0 1200 60"
+                            preserveAspectRatio="none">
+                            <path
+                                d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0" />
                         </svg>
-                        <span className="input input--nao">
-                            <input className="input__field input__field--nao" type="text" value={search} onChange={e => setSearch(e.target.value)} id="input-1" placeholder='"Distopi"' />
-                            <svg className="graphic graphic--nao" width="300%" height="100%" viewBox="0 0 1200 60"
-                                preserveAspectRatio="none">
-                                <path
-                                    d="M0,56.5c0,0,298.666,0,399.333,0C448.336,56.5,513.994,46,597,46c77.327,0,135,10.5,200.999,10.5c95.996,0,402.001,0,402.001,0" />
-                            </svg>
-                        </span>
-                    </div>
-                    {/* <div className="col-md-6">
+                    </span>
+                </div>
+
+                {/* <div className="col-md-6">
                         <div className="categories">
                             <ul>
                                 <li>
@@ -185,9 +181,7 @@ const Books = ({ books }) => {
                             </ul>
                         </div>
                     </div> */}
-                </div>
-
-            </motion.div>
+            </motion.div >
 
             <div className="row">
                 {filteredList.map(book => (
@@ -203,7 +197,7 @@ const Books = ({ books }) => {
 
 
 
-        </motion.div>
+        </motion.div >
     )
 }
 
